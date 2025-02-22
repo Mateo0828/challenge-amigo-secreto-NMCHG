8eee04af2e0f4035a247f1ee1d80f97e77efe6a9
Nicolas Mateo Chavez Garcia <nicolasmateochavezgarcia10@gmail.com>
2025-02-22 13:11:58 -0500

    Agrego todas las funcionalidades del programa para que funcione y se ejecute correctamente

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value.trim(); 
+
+    if (amigo !== '') {
+        amigos.push(amigo);
+        actualizarListaAmigos(); 
+        limpiarCaja();
+    } else {
+        alert("Por favor, ingresa un nombre antes de añadir.");
+    }
+}
+
+function actualizarListaAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''; 
+
+    for (let i = 0; i < amigos.length; i++) {
+        let elementoLista = document.createElement('li');
+        elementoLista.textContent = amigos[i];
+        listaAmigos.appendChild(elementoLista);
+    }
+}
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
+
+function sortearAmigo() {
+    if (amigos.length > 0) {
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAleatorio];
+
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `<li> El amigo sorteado es: <strong>${amigoSorteado}</strong></li>`;
+    } else {
+        document.getElementById('resultado').innerHTML = '<li>No hay amigos para sortear.</li>';
+    }
+}
